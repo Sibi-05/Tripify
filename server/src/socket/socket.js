7b8eed76000bc8f29c771ff6c4462c58b6e5acac
sibi <ssibi769@gmail.com>
2026-07-06 22:21:59 +0530

    change in backend origin

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://tripify-web.netlify.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   },
